bbcafb1d8574469c2f6761582b761725a4d594d5
sharma-tanishq <sharma.tanishq9999@gmail.com>
2022-06-09 02:08:46 +0530

    added get reports request

---
+++ readme.md
@@ -0,0 +1,124 @@
+# Steps to setup Locally
+Clone the Repository: `git clone "url of this repo"`
+Install MongoDB (if not installed)
+Run MongoDB server
+Change the `mongooseURI` in `connectDB.js` according to your local mongo server
+
+## Test Cases :
+POST request at `http://localhost:5000/api/reports`:
+Request 1 :
+{
+  "reportDetails": {
+    "userID": "user-1",
+    "marketID": "market-1",
+    "marketName": "Vashi Navi Mumbai",
+    "cmdtyID": "cmdty-1",
+    "marketType": "Mandi",
+    "cmdtyName": "Potato",
+    "priceUnit": "Pack",
+    "convFctr": 50,
+    "price": 700
+  }
+}
+Response:
+{
+    "status": "success",
+    "reportID": "62a0f17e8a57dc9246018c3f"
+}
+
+Request 2:
+{ 
+    "reportDetails": {
+      "userID": "user-2",
+      "marketID": "market-1",
+      "marketName": "Vashi Navi Mumbai",
+      "cmdtyID": "cmdty-1",
+      "cmdtyName": "Potato",
+      "priceUnit": "Quintal",
+      "convFctr": 100,
+      "price": 1600
+    }
+}
+Response:
+{
+    "status": "success",
+    "reportID": "62a0f17e8a57dc9246018c3f"
+}
+
+
+Request 3(same as Request 1  i..e the userId is already present in the report that is being reffered):
+{
+  "reportDetails": {
+    "userID": "user-1",
+    "marketID": "market-1",
+    "marketName": "Vashi Navi Mumbai",
+    "cmdtyID": "cmdty-1",
+    "marketType": "Mandi",
+    "cmdtyName": "Potato",
+    "priceUnit": "Pack",
+    "convFctr": 50,
+    "price": 700
+  }
+}
+
+Response:
+{
+    "status": "failed",
+    "message": "User has sent request already"
+}
+
+Request 4 (If the request does not contain all values here does not contain userID):
+{
+  "reportDetails": {
+    "marketID": "market-1",
+    "marketName": "Vashi Navi Mumbai",
+    "cmdtyID": "cmdty-1",
+    "marketType": "Mandi",
+    "cmdtyName": "Potato",
+    "priceUnit": "Pack",
+    "convFctr": 50,
+    "price": 700
+  }
+}
+
+Response: Bad request userID not present
+
+
+
+GET request at `http://localhost:5000/api/reports`:
+
+Request 1: http://localhost:5000/api/reports?reportID=62a0f17e8a57dc9246018c3f
+
+Response:
+{
+    "_id": "62a0f17e8a57dc9246018c3f",
+    "cmdtyID": "cmdty-1",
+    "cmdtyName": "Potato",
+    "marketID": "market-1",
+    "marketName": "Vashi Navi Mumbai",
+    "users": [
+        "user-1",
+        "user-2"
+    ],
+    "priceUnit": "Kg",
+    "price": 15,
+    "timestamp": 1654714786100,
+    "__v": 1
+}
+
+Request 2 (sending wrong reportID and wrong format): http://localhost:5000/api/reports?reportID=aaaaaaaaaaaaaa
+
+Response: Bad Request
+
+Request 3 (sending wrong reportID but correct format): http://localhost:5000/api/reports?reportID=62a0f17e8a57dc9246018c3e
+
+Response: Report Not found
+
+
+
+## Technologies used :
+
+1) Node.js
+2) Express
+3) JavaScript
+
--- routes/requests.js
+++ routes/requests.js
@@ -1,30 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const Report = require('../models/report');
+const mongoose=require('mongoose');
+
 //request validator middleware to to verify request before processing
-const requestUser = require('../middleware/requestValidator');
+const requestValidator = require('../middleware/requestValidator');
 
 
-router.post('/reports', requestUser, async (req, res) => {
+router.post('/reports', requestValidator, async (req, res) => {
     let status = "failed";
-    const currentDate= new Date();
+    const currentDate = new Date();
     try {
         //checking wether cmdtyId,marketId already present or not
         let report = await Report.findOne({ cmdtyID: req.body.reportDetails.cmdtyID, marketID: req.body.reportDetails.marketID });
         //if present
         if (report) {
             //if the user has already sent request of same cmdty before
-            if(report.users.includes(req.body.reportDetails.userID)){
-                return res.status(400).json({status:status,message:"User has sent request already"})
+            if (report.users.includes(req.body.reportDetails.userID)) {
+                return res.status(400).json({ status: status, message: "User has sent request already" })
             }
             try {
-                report.timestamp=currentDate.getTime();
+                report.timestamp = currentDate.getTime();
                 await report.save();
 
-                let price=req.body.reportDetails.price/req.body.reportDetails.convFctr;
-                price +=report.users.length * report.price;;
-                price/=report.users.length+1;
-                report.price=price;
+                let price = req.body.reportDetails.price / req.body.reportDetails.convFctr;
+                price += report.users.length * report.price;;
+                price /= report.users.length + 1;
+                report.price = price;
                 await report.save();
 
                 report.users.push(req.body.reportDetails.userID);
@@ -32,41 +34,65 @@ router.post('/reports', requestUser, async (req, res) => {
 
             } catch (error) {
                 console.log(error);
-                res.status(500).json({status:status,message:"Unable to create report"})
+                res.status(500).json({ status: status, message: "Unable to create report" })
             }
-            status="success";
-            res.status(200).json({status:status,reportID:report.id})
+            status = "success";
+            return res.status(200).json({ status: status, reportID: report.id })
         }
         //creating new report
         else {
             try {
-                let price=req.body.reportDetails.price/req.body.reportDetails.convFctr;
+                let price = req.body.reportDetails.price / req.body.reportDetails.convFctr;
                 report = await Report.create({
-                    cmdtyID:req.body.reportDetails.cmdtyID,
-                    cmdtyName:req.body.reportDetails.cmdtyName,
-                    marketID:req.body.reportDetails.marketID,
-                    marketName:req.body.reportDetails.marketName,
-                    priceUnit:"Kg",
-                    price:price,
-                    users:[req.body.reportDetails.userID],
-                    timestamp:currentDate.getTime(),
+                    cmdtyID: req.body.reportDetails.cmdtyID,
+                    cmdtyName: req.body.reportDetails.cmdtyName,
+                    marketID: req.body.reportDetails.marketID,
+                    marketName: req.body.reportDetails.marketName,
+                    priceUnit: "Kg",
+                    price: price,
+                    users: [req.body.reportDetails.userID],
+                    timestamp: currentDate.getTime(),
                 })
-                if(!report){
-                    res.status(500).json({status:status,message:"Unable to create report"})
+                if (!report) {
+                    return res.status(500).json({ status: status, message: "Unable to create report" })
                 }
-                else{
-                    status="success";
-                    res.status(200).json({status:status,reportID:report.id})
+                else {
+                    status = "success";
+                    return res.status(200).json({ status: status, reportID: report.id })
                 }
             } catch (error) {
                 console.log(error);
-                res.status(500).json({status:status,message:"Unable to create report"})
+                return res.status(500).json({ status: status, message: "Unable to create report" })
             }
-            
+
         }
     } catch (error) {
         console.log(error);
         return res.status(500).json({ status: status, message: "Internal Server Error" })
+    }
+})
+
+router.get('/reports', async (req, res) => {
+    try {
+
+        let reportID=req.query.reportID.toString().replace(/\n/g, '')
+
+        if(!mongoose.isValidObjectId(reportID)){
+            return res.status(400).send("Bad Request");
+        }
+
+        let report=await Report.findById(reportID);
+        if(!report){
+
+            return res.status(404).send("Report Not found");
+        }
+        else{
+            return res.status(200).json(report);
+        }
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error" });
     }
 })
 module.exports = router
